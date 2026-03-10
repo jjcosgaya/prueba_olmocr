c00ed7f0799495568965c5287a4895464a855951
jota <jotalabota@gmail.com>
2026-03-10 15:40:24 +0200

    fix: use version 3.11 python

--- init.sh
+++ init.sh
@@ -4,5 +4,6 @@ apt install curl -y
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
 uv init
+uv python pin 3.11
 uv add olmocr[gpu]
 uv add https://download.pytorch.org/whl/cu128/flashinfer/flashinfer_python-0.2.5%2Bcu128torch2.7-cp38-abi3-linux_x86_64.whl
